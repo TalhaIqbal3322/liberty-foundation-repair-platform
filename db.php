d49a5e5b050a067c858348a68fd6899bc4c2f0ac
Talha-Iqbal293 <talhaiqbal.digitalnexus@gmail.com>
2025-07-19 18:02:59 +0500

    Add database connection file

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+// $conn=mysqli_connect('localhost','root','','lbr');
+$conn=mysqli_connect('localhost','lbr','l2iX+UOVoWDN','lbr');
+
+
+$timezone = "America/New_York";
+date_default_timezone_set($timezone);
+
+
