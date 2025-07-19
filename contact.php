58c656cd8e84b4c8d88fca4112407033856cdaa8
Talha-Iqbal293 <talhaiqbal.digitalnexus@gmail.com>
2025-07-19 18:02:25 +0500

    Add Contact page

---
+++ contact.php
@@ -0,0 +1,298 @@
+<?php include 'header.php';?>
+
+<body class="body-1">
+
+    <?php include 'navbar.php';?>
+
+    <!-- Header area end -->
+    <main>
+        <!-- Breadcrumb area start  -->
+        <div class="breadcrumb__area breadcrumb-space overly theme-bg-heading-primary overflow-hidden">
+            <div class="breadcrumb__background" data-background="assets/imgs/breadcrumb/breadcrumb.jpg"></div>
+            <div class="container">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-12">
+                        <div class="breadcrumb__content text-center">
+                            <h1 class="breadcrumb__title color-white title-animation">Contact Us</h1>
+                            <div class="breadcrumb__menu d-inline-flex justify-content-center">
+                                <nav>
+                                    <ul>
+                                        <li>
+                                            <span>
+                                                <a href="index.php">
+                                                    <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M1 5.9L7.5 1L14 5.9V13.6C14 13.9713 13.8478 14.3274 13.5769 14.5899C13.306 14.8525 12.9386 15 12.5556 15H2.44444C2.06135 15 1.69395 14.8525 1.42307 14.5899C1.15218 14.3274 1 13.9713 1 13.6V5.9Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                        <path d="M5.33398 15V8H9.66732V15" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+
+                                                    Home
+                                                </a>
+                                            </span>
+                                        </li>
+                                        <li class="active"><span>Contact Us</span></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Breadcrumb area start  -->
+
+        <!-- "contact-us  area start -->
+        <section class="contact-us section-space">
+            <div class="container">
+                <div class="row mb-minus-30">
+                    <div class="col-md-6 col-lg-4">
+                        <div class="contact-us__info-item">
+                            <div class="contact-us__icon">
+                                <img src="assets/imgs/contact-us/location.svg" alt="image not found">
+                            </div>
+                            <div class="contact-us__text">
+                                <h4 class="text-capitalize mb-10">Our Location</h4>
+                                <a href="#">15128 SH 110 S Whitehouse, TX 75791</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="contact-us__info-item">
+                            <div class="contact-us__icon">
+                                <img src="assets/imgs/contact-us/email.svg" alt="image not found">
+                            </div>
+                            <div class="contact-us__text">
+                                <h4 class="text-capitalize mb-10">Email address</h4>
+                                <a href="mailto:muj287151@gmail.com">example@exdosstudio.com</a>
+                                <a href="muj287151@gmail.com">muj287151@gmail.com</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="contact-us__info-item">
+                            <div class="contact-us__icon">
+                                <img src="assets/imgs/contact-us/phone.svg" alt="image not found">
+                            </div>
+                            <div class="contact-us__text">
+                                <h4 class="text-capitalize mb-10">Phone number</h4>
+                                <a href="tel:+9037145012">(+903) 714 - 5012</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- "contact-us  area end -->
+
+        <!--contact-map-->
+        <div class="contact-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14600.157880481716!2d90.35107202273903!3d23.81719545680701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c12c1b289e39%3A0x8d3bdfb80d3ebe88!2sRd%20No.%204%2C%20Dhaka%201216!5e0!3m2!1sen!2sbd!4v1701837409513!5m2!1sen!2sbd" height="530" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <!--contact-map-->
+
+        <!--get-in-touch-->
+        <section class="get-in-touch get-in-touch__space">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="get-in-touch__text">
+                            <h2>Contact Us</h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+
+                        <div class="get-in-touch__contact-form">
+                            <h3 class="mb-20">Get in touch</h3>
+
+                            <div class="row">
+                                <div class="col-xl-6">
+                                    <div class="request-a-quote__form-input">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <input required name="looking" id="looking" type="text" placeholder="I am looking for">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="col-xl-6">
+                                    <div class="request-a-quote__form-input">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <input name="postal_code" id="postal_code" type="text" required placeholder="Postal Code">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-xl-6">
+                                    <div class="request-a-quote__form-input">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <input name="datetime" required id="datetime" type="datetime-local" placeholder="Postal Code">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-xl-6">
+                                    <div class="request-a-quote__form-input">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <input name="name" required id="name" type="" placeholder="First & Last Name">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="col-xl-6">
+                                    <div class="request-a-quote__form-input">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <input name="email" required id="email" type="email" placeholder="Email">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="col-xl-6">
+                                    <div class="request-a-quote__form-input">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <input name="phone_no" id="phone_no" type="" placeholder="Phone No">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-xl-12">
+                                    <div class="request-a-quote__form-input">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <input name="address" id="address" type="" placeholder="Address">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-12">
+                                    <div class="request-a-quote__form-input textarea">
+                                        <div class="validation__wrapper-up position-relative">
+                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M8 15H15" stroke="#6A6A6A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M11.5 1.50805C11.8094 1.18275 12.2291 1 12.6667 1C12.8833 1 13.0979 1.04487 13.2981 1.13204C13.4982 1.21921 13.6801 1.34698 13.8333 1.50805C13.9865 1.66912 14.1081 1.86034 14.191 2.07079C14.2739 2.28124 14.3166 2.5068 14.3166 2.73459C14.3166 2.96238 14.2739 3.18794 14.191 3.39839C14.1081 3.60884 13.9865 3.80006 13.8333 3.96113L4.11111 14.1823L1 15L1.77778 11.7292L11.5 1.50805Z" stroke="#6A6A6A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                            <textarea name="textarea" id="comment" placeholder="Comment"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <button id="submit_btn" type="submit" class="rr-btn">
+                                        <span class="btn-wrap">
+                                            <span class="text-one">Sumit Message</span>
+                                            <span class="text-two">Sumit Message</span>
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--get-in-touch-->
+
+        <!-- our-clients area end -->
+    </main>
+
+
+    <?php include 'footer.php';?>
+
+
+
+
+    <script type="text/javascript">
+   
+
+        $(document).ready(function () {
+          $('#submit_btn').on('click', function () {
+
+            $("#submit_btn").attr("disabled", "disabled");
+
+            var looking = $('#looking').val().trim();
+            var postal_code = $('#postal_code').val().trim();
+            var datetime = $('#datetime').val().trim();
+            var name = $('#name').val().trim();
+            var email = $('#email').val().trim();
+            var phone_no = $('#phone_no').val().trim();
+            var address = $('#address').val().trim();
+            var comment = $('#comment').val().trim();
+
+            var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            var phonePattern = /^\d{10}$/;
+
+            if (looking !== "" && postal_code !== "" && datetime !== "" && name !== "" && email !== "" && phone_no !== "" && address !== "" && comment !== "") {
+
+
+                 $.ajax({
+              url: "contact-submit.php",
+              type: "POST",
+              data: {
+                looking: looking,
+                postal_code: postal_code,
+                datetime: datetime,
+                name: name,
+                email: email,
+                phone_no: phone_no,
+                address: address,
+                comment: comment
+              },
+              cache: false,
+              success: function (response) {
+
+                  var dataResult = JSON.parse(response);
+
+       
+            
+
+                  if (dataResult.statusCode == 200) {
+                    Swal.fire({
+                      icon: "success",
+                      title: "Success",
+                      text: "Request has been sent successfully. We will contact you soon"
+                    }).then((result) => {
+                      if (result.isConfirmed) {
+                        window.location.href = '';
+                      }
+                    });
+
+                  } else {
+                    showError(dataResult.message || "An unexpected error occurred.");
+                  }
+              }
+              });
+
+
+
+
+              }
+              
+
+              else {
+                  showError('Please fill all fields.');
+              }
+
+
+
+          });
+
+
+
+          function showError(message) {
+              Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: message
+            });
+              $("#submit_btn").removeAttr("disabled");
+          }
+
+      });
+  </script>
